15f688927d2e17e2d2ceb628499801e80ef664ae
VinayBandaru07 <vinaybandarug@gmail.com>
2024-12-02 14:36:49 +0530

    Fixed the completed orders layout

--- src/pages/admin/CompletedOrders.tsx
+++ src/pages/admin/CompletedOrders.tsx
@@ -98,8 +98,8 @@ function CompletedOrders() {
 
   return (
     <div className="space-y-6">
-      <div className="flex justify-between items-center">
-        <h2 className="text-2xl font-bold text-gray-900">Completed Orders</h2>
+      <div className="flex justify-around items-center mx-auto">
+        <h2 className="lg:text-2xl md:text-2xl  font-bold text-gray-900 hidden lg:block md:block">Completed Orders</h2>
         <div className="flex items-center space-x-4">
           <div className="flex items-center space-x-2">
             <Calendar className="w-5 h-5 text-gray-500" />
@@ -115,7 +115,7 @@ function CompletedOrders() {
             className="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-purple-600 hover:bg-purple-700"
           >
             <Download className="w-4 h-4 mr-2" />
-            Export Orders
+           <span className=''> Export </span>
           </button>
         </div>
       </div>
